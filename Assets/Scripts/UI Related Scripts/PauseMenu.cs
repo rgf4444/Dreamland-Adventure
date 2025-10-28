50f71a41a533910792348533b2d3d15869d77b27
EyeRiz <iris.odon59@gmail.com>
2025-10-28 18:49:15 +0800

    Pause Menu added
    pause menu done (Forgot to add dizzy animatino on the enemies oopx lemme add it)

---
+++ Assets/Scripts/UI Related Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    [Header("Scene Names")]
+    public string levelSelectScene = "LevelSelect";
+    public string currentGameScene = "GameScene";
+
+    // Public methods that buttons can call directly
+    public void ResumeGame()
+    {
+        Debug.Log("Resuming Game...");
+        // gameObject.SetActive(false);
+        // Time.timeScale = 1f;
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Restarting Game...");
+        SceneManager.LoadScene(currentGameScene);
+    }
+
+    public void GoToLevelSelect()
+    {
+        Debug.Log("Going to Level Select...");
+        SceneManager.LoadScene(levelSelectScene);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game...");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
