ca0901d4bd6464ea57df0a7bceb484d5746a08f1
rgf4444 <rongabriellef@gmail.com>
2025-10-29 08:16:27 +0800

    door

--- Assets/Scripts/UI Related Scripts/LoadSceneOnTrigger.cs
+++ Assets/Scripts/UI Related Scripts/LoadSceneOnTrigger.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadSceneOnTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Scene Settings")]
+    public string sceneToLoad;
+
+    [Header("Player Tag")]
+    public string playerTag = "Player";
+
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (hasTriggered) return;
+
+        if (other.CompareTag(playerTag))
+        {
+            hasTriggered = true;
+            Debug.Log("Loading scene: " + sceneToLoad);
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Optional: Use OnTriggerEnter for 3D instead of OnTriggerEnter2D
 }
